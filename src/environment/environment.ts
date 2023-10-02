aa3db2ea5f2711d9a44252cfc59e3f5b67273f77
John Danquah-Boateng <yjbdanquah@gmail.com>
2023-10-02 21:01:01 +0000

    add firebase hosting and github actions

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDhgrpt_NyWuNNer6-KyedhXLcFCWpjkPc",
-    authDomain: "asuoyeboahsda.firebaseapp.com",
-    projectId: "asuoyeboahsda",
-    storageBucket: "asuoyeboahsda.appspot.com",
-    messagingSenderId: "1061609643607",
-    appId: "1:1061609643607:web:650062fb8c2b255711cd12",
-    measurementId: "G-W5PZ1NGZE7"
+    apiKey: "AIzaSyAgjb4ryUpmmKQWIPvH1YfrvLIy33arKCM",
+    authDomain: "asuoyeboah-sda.firebaseapp.com",
+    projectId: "asuoyeboah-sda",
+    storageBucket: "asuoyeboah-sda.appspot.com",
+    messagingSenderId: "890346768000",
+    appId: "1:890346768000:web:56363cb1f08dfec48e0865"
   },
   api: {
     getRandomBibleQuotes: 'https://labs.bible.org/api/?passage=random&type=json'
